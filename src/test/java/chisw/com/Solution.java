c322362556b260618dee3b2d2681e66a00890988
roman.streltsov <roman.streltsov@citrix.com>
2018-07-11 17:47:00 +0300

    changes 1

--- src/test/java/chisw/com/Solution.java
+++ src/test/java/chisw/com/Solution.java
@@ -12,58 +12,21 @@ public class Solution
 {
 
     @Test
-    public void AssertionTest ()
-    {
-
+    public void AssertionTest () throws IOException {
+        InputData test = new InputData();
+        test.readInputData();
     }
 }
 
 
 class InputData {
 
-    private int a;
-    private int b;
-    private int c;
-
-    public int getA() {
-        return a;
-    }
-
-    public int getB() {
-        return b;
-    }
-
-    public int getC() {
-        return c;
-    }
-
-    public void setA(int a) {
-        this.a = a;
-    }
-
-    public void setB(int b) {
-        this.b = b;
-    }
-
-    public void setC(int c) {
-        this.c = c;
-    }
-
     public void readInputData() throws IOException {
         List<String> list = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new FileReader(new File("C:\\project1\\taskup1\\Object_task_up_input.log")));
         String str;
         list = reader.lines().collect(Collectors.toList());
-        //list.forEach(System.out::println);
+        list.forEach(System.out::println);
 
-
-        String[][] array = new String[list.size() - 1][3];
-
-        for (int i = 1; i < list.size(); i++) {
-            for (int j = 0; j <= 3; j++) {
-                array[i][j] = list.get(i);
-                System.out.println(array[i][j]);
-            }
-        }
     }
 }
