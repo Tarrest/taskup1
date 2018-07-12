08ec503ac4db4a156c1e5c432a60c2a731cd8da0
roman.streltsov <roman.streltsov@citrix.com>
2018-07-12 15:09:34 +0300

    test1

--- src/test/java/chisw/com/Solution.java
+++ src/test/java/chisw/com/Solution.java
@@ -4,15 +4,15 @@ import org.junit.Test;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 
-public class Solution
-{
+public class Solution {
 
     @Test
-    public void AssertionTest () throws IOException {
+    public void AssertionTest() throws IOException {
         InputData test = new InputData();
         test.readInputData();
     }
@@ -21,12 +21,32 @@ public class Solution
 
 class InputData {
 
+    private double sum = 0;
+    private List<String> listInput;
+    private List<String> listRezExp;
+    private List<String> listRez;
+    private BufferedReader reader;
+
     public void readInputData() throws IOException {
-        List<String> list = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new FileReader(new File("C:\\project1\\taskup1\\Object_task_up_input.log")));
-        String str;
-        list = reader.lines().collect(Collectors.toList());
-        list.forEach(System.out::println);
+        listInput = new ArrayList<>();
+        reader = new BufferedReader(new FileReader(new File("C:\\project1\\taskup1\\Object_task_up_input.log")));
+        listInput = reader.lines().collect(Collectors.toList());
+        System.out.println("rez");
+        for (int i = 1; i < listInput.size(); i++) {
+            String str = listInput.get(i).toString();
+            double[] nums = Arrays.stream(str.split("\t")).mapToDouble(Double::parseDouble).toArray();
+            for (int j = 0; j < nums.length; j++) {
+                sum += nums[j];
+            }
+
+            if (sum > 1000) {
+                System.out.println("5");
+            } else {
+                System.out.println("6");
+            }
+            sum = 0;
+
+        }
 
     }
 }
