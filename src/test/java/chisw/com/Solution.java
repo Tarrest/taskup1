93ad21817aa6901dc74df908666caa5ddcca0707
roman.streltsov <roman.streltsov@citrix.com>
2018-07-12 16:57:04 +0300

    test2

--- src/test/java/chisw/com/Solution.java
+++ src/test/java/chisw/com/Solution.java
@@ -22,28 +22,31 @@ public class Solution {
 class InputData {
 
     private double sum = 0;
+    private int rez;
     private List<String> listInput;
     private List<String> listRezExp;
-    private List<String> listRez;
+    private List<Integer> listRez;
     private BufferedReader reader;
 
     public void readInputData() throws IOException {
         listInput = new ArrayList<>();
         reader = new BufferedReader(new FileReader(new File("C:\\project1\\taskup1\\Object_task_up_input.log")));
         listInput = reader.lines().collect(Collectors.toList());
-        System.out.println("rez");
         for (int i = 1; i < listInput.size(); i++) {
             String str = listInput.get(i).toString();
             double[] nums = Arrays.stream(str.split("\t")).mapToDouble(Double::parseDouble).toArray();
             for (int j = 0; j < nums.length; j++) {
                 sum += nums[j];
             }
-
             if (sum > 1000) {
-                System.out.println("5");
+                rez = 5;
             } else {
-                System.out.println("6");
+                rez = 6;
             }
+            listRez = new ArrayList<>();
+            listRez.add(rez);
+            listRez.forEach(System.out::println);
+
             sum = 0;
 
         }
